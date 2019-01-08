e8698c4ce55ced3ef089067beaa008eadf1d7042
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-01-08 12:03:14 +0100

    Change library to liac_arff
    Because the one in Pandas does not read STRING.... deplorable..

--- util/_file_loader.py
+++ util/_file_loader.py
@@ -1,11 +1,17 @@
-from scipy.io import arff
+# from scipy.io import arff
+# from Lib import *
+import arff
+
 #from pandas import pandas
 
-file_path = 'C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/consolidated_Final_Experiment_1.arff'
-for row in arff.loadarff(file_path):
-    print(row)
+file_path = 'C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/consolidated_Final_Experiment_1_ANSI.arff'
 
-# def _load_arff(file_path):
-#     data, meta = arff.loadarff(file_path)
-#     df = pandas.DataFrame(data[0])
-#     df.head()
+# with open(file_path) as file:
+#     print(file.readline())
+
+# file_object_test = open("C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/weather.arff")
+
+file_object = open(file_path)
+file_content = file_object.read()
+print(file_content)
+data = arff.loads(file_content,encode_nominal=True)
