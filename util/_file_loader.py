66c5c8ff5db18ad031d96bda1da8936631bb34db
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-01-23 00:26:12 +0100

    labeled dataframes

--- util/_file_loader.py
+++ util/_file_loader.py
@@ -1,7 +1,7 @@
 #from scipy.io import arff #String attributes not supported by scipy.io arff...
 # from Lib import *
 import arff, numpy as np
-#import pandas as pd
+import pandas as pd
 
 #from pandas import pandas
 
@@ -17,24 +17,19 @@ file_path +=file_1
 
 file_object = open(file_path)
 file_content = file_object.read()
-#print(file_content)
 dataset = arff.loads(file_content,encode_nominal=True,return_type=arff.DENSE)
-#data = np.array(dataset['data'])
 print(dataset['description'])
 print(dataset['relation'])
 print(dataset['attributes'])
 print(dataset['data'][0])
+print("numer of lines".+ dataset['data'].__len__())
 
-print(dataset['data'].__len__())
+#label data frame columns
+#https://pandas.pydata.org/pandas-docs/stable/10min.html
+df = pd.DataFrame(dataset['data'])
+df_labels = pd.DataFrame(dataset['attributes'])
+df.rename(index=str,columns=df_labels[0]) 
 
-
-#for row in arff.loads(file_content,encode_nominal=True):
- #   print(row)
-  #  print(row[-1])
-
-#print(list(arff.loads(file_content,encode_nominal=True,return_type=arff.DENSE)))
-
-#df = pd.DataFrame(dt[0])
-#df.describe()
-
+#Compute correlations 
+#https://www.datascience.com/learn-data-science/fundamentals/introduction-to-correlation-python-data-science
 
