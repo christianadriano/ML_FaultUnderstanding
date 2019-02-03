920aac676ea85cbe0932a4ca0f080448e30460de
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-03 14:27:08 +0100

    Compute recruitment of high skilled participants

--- analysis/descriptive/RecruitmentStatistics.py
+++ analysis/descriptive/RecruitmentStatistics.py
@@ -32,9 +32,9 @@ class RecruitmentStatistics(object):
         Computes the number and proportion of participants who qualified with above minimum grade,
         75% for experiment-1 and 80% for experiment-2
         '''
-        high_skill_flags_1 = self.df_1['score']>=3
-        high_skill_flags_2 = self.df_1['score']>=4
-        
+        high_skill_flags_1 = self.df_1['grade']>=3
+        high_skill_flags_2 = self.df_2['grade']>=4
+
         hs_df_1 = self.df_1[high_skill_flags_1]
         hs_df_2 = self.df_2[high_skill_flags_2]
         
@@ -42,16 +42,16 @@ class RecruitmentStatistics(object):
         hs_df_1_count = hs_df_1['worker_id'].nunique()
         hs_df_2_count = hs_df_2['worker_id'].nunique()
         
-        total_df_1 = self.df1['worker_id'].nunique()
-        total_df_2 = self.df2['worker_id'].nunique()
-        
-        hs_df1_proportion = 100 * hs_df_1 / total_df_1
-        hs_df2_proportion = 100 * hs_df_2 / total_df_2
+        total_df_1 = self.df_1['worker_id'].nunique()
+        total_df_2 = self.df_2['worker_id'].nunique()
+                
+        hs_df1_proportion = 100 * hs_df_1_count / total_df_1
+        hs_df2_proportion = 100 * hs_df_2_count / total_df_2
         
         print("High skill recruitment rates (above minimum of 50% exp.1 and 60% exp.2")
         print("Experiment-1, "+str(hs_df1_proportion)+ " out of " +str(total_df_1)+" qualified")
         print("Experiment-2, "+str(hs_df2_proportion)+ " out of " +str(total_df_2)+" qualified")
-
+        
 #Main execution
 recruitmentStats = RecruitmentStatistics()
 recruitmentStats.high_skill_rate()
