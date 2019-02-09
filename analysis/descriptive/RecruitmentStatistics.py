a595de759c96386b0c1fd9ace0f6ef92e66226e2
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-09 15:33:41 +0100

    statistical significant test age differences
    Shapiro-Wilk test p-value = 2.586198052248272e-15
    data is probably not normal
    Shapiro-Wilk test p-value = 1.202891526846171e-18
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    RanksumsResult(statistic=1.4501229547853733, pvalue=0.14702423496490485)
    The p-value>0.05, so we cannot say anything about possible differences in mean age
    E1 mean=29.267634854771785
    E1 median=28.0
    E2 mean=30.388329979879277
    E2 median=28.0

--- analysis/descriptive/RecruitmentStatistics.py
+++ analysis/descriptive/RecruitmentStatistics.py
@@ -136,6 +136,40 @@ class RecruitmentStatistics(object):
      
         return(df_series1.age,df_series2.age)
  
+ 
+    def test_age_averages(self, series_1, series_2):
+        ''' 
+        Evaluate if average rates are different
+        '''
+            
+        #test if rate series_1 normally distributed
+        result = morestats.shapiro(series_1)
+        print("Shapiro-Wilk test p-value = "+str(result[1]))
+        if(result[1]<0.05):
+            print("data is probably not normal")
+        #test if rate series_2 normally distributed
+        result = morestats.shapiro(series_2)
+        print("Shapiro-Wilk test p-value = "+str(result[1]))
+        if(result[1]<0.05):
+            print("series_1 is probably not normal")
+        print("data is probably not normal")
+        
+        #Run Wilcoxon rank sum test
+        result = stats.ranksums(series_2,series_1)
+        print("Wilcoxon test:")
+        print(result)
+        print("The p-value>0.05, so we cannot say anything about possible differences in mean age")
+
+        exp1_mean = np.mean(series_1)
+        print("E1 mean="+str(exp1_mean))
+        exp1_median = np.median(series_1)
+        print("E1 median="+str(exp1_median))
+        
+        exp2_mean = np.mean(series_2)
+        print("E2 mean="+str(exp2_mean))
+        exp2_median = np.median(series_2)
+        print("E2 median="+str(exp2_median))
+ 
     '''
     Controller of main execution
     '''    
@@ -143,3 +177,4 @@ recruitmentStats = RecruitmentStatistics()
 #recruitmentStats.high_skill_rate()
 #recruitmentStats.gender_distribution()
 series_1, series_2 = recruitmentStats.age_distribution()
+recruitmentStats.test_age_averages(series_1,series_2)
