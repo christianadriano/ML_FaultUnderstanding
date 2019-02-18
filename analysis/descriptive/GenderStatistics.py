751e279f9817220343745a05533e271d26c7e9ef
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-18 11:43:54 +0100

    Compute stats for all in E2 by profession
    series_1 = Male, series_2 = Female
    Professional_Developer
    Shapiro-Wilk test p-value = 1.824668714733857e-11
    data is probably not normal
    Shapiro-Wilk test p-value = 5.382283418953193e-08
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=-0.8444408680028249, p-value =0.3984230933414332
    The p-value>0.05, so we cannot say anything about possible differences in mean
    series_1 mean=32.5, median=31.0
    series_2 mean=31.581196581196583, median=30.0
    Hobbyist
    Shapiro-Wilk test p-value = 1.22421208645521e-16
    data is probably not normal
    Shapiro-Wilk test p-value = 2.270871268805763e-10
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=2.765263788371809, p-value =0.005687678097484053
    The p-value<=0.05 shows that the series 1 and 2 have distinct means in 95% of the time
    series_1 mean=31.687350835322196, median=30.0
    series_2 mean=34.121212121212125, median=31.0
    Graduate_Student
    Shapiro-Wilk test p-value = 9.145799072704497e-16
    data is probably not normal
    Shapiro-Wilk test p-value = 7.843347304969939e-09
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=-0.04232831882198099, p-value =0.9662369703559458
    The p-value>0.05, so we cannot say anything about possible differences in mean
    series_1 mean=27.752136752136753, median=26.0
    series_2 mean=28.208695652173912, median=26.0
    Undergraduate_Student
    Shapiro-Wilk test p-value = 8.100080121641818e-18
    data is probably not normal
    Shapiro-Wilk test p-value = 3.682591358185371e-15
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=3.152744372791378, p-value =0.00161743377297487
    The p-value<=0.05 shows that the series 1 and 2 have distinct means in 95% of the time
    series_1 mean=24.549382716049383, median=23.0
    series_2 mean=26.854625550660792, median=24.0
    Other
    Shapiro-Wilk test p-value = 3.597334341520764e-08
    data is probably not normal
    Shapiro-Wilk test p-value = 0.00016282089927699417
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=0.7890481585693991, p-value =0.4300838598854517
    The p-value>0.05, so we cannot say anything about possible differences in mean
    series_1 mean=35.86138613861386, median=31.0
    series_2 mean=36.11363636363637, median=34.0
    All
    Shapiro-Wilk test p-value = 4.723126427451404e-31
    data is probably not normal
    Shapiro-Wilk test p-value = 2.128882390395371e-22
    series_1 is probably not normal
    data is probably not normal
    Wilcoxon test:
    W-statistic=2.06101605297458, p-value =0.039301508617949066
    The p-value<=0.05 shows that the series 1 and 2 have distinct means in 95% of the time
    series_1 mean=29.876955903271693, median=28.0
    series_2 mean=30.970437017994858, median=29.0

---
+++ analysis/descriptive/GenderStatistics.py
@@ -0,0 +1,107 @@
+'''
+Created on Feb 18, 2019
+
+@author: Christian Adriano
+'''
+
+from util._file_loader import FileLoader
+from util.StatisticalSignificanceTest import StatisticalSignificanceTest
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from scipy.stats import morestats,stats
+import csv
+
+class GenderStatistics(object):
+    '''
+    Computer gender distribution by profession, years of experience, and age
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        loader = FileLoader()
+        self.df_1 = loader._load_file_1()
+        self.df_2 = loader._load_file_2()
+        
+        '''
+        remove duplicates and people who did not qualify
+        '''
+        qualified_flags_1 = self.df_1['qualification_score']>=2 
+        qualified_flags_2 = self.df_2['qualification_score']>=3
+        q_df1 = self.df_1[qualified_flags_1]
+        q_df2 = self.df_2[qualified_flags_2]
+        self.df_1 = q_df1[['gender','worker_id','age']].drop_duplicates(keep='last')
+        self.df_2 = q_df2[['gender','worker_id','experience','age']].drop_duplicates(keep='last')
+        
+    def gender_distribution(self):
+        '''
+        Computes the number and proportion of participants from each gender.
+        This will be both for qualified and not qualified participants.
+        '''
+       
+    
+        #count Exp-1
+        females_1 = self.df_1[self.df_1.gender==0].shape[0]
+        males_1 = self.df_1[self.df_1.gender==1].shape[0]
+        prefer_not_tell_1 = self.df_1[self.df_1.gender==2].shape[0]
+        
+        print("Experiment-1 results")
+        print("females="+str(females_1))
+        print("males="+str(males_1))
+        print("prefer_not_tell="+str(prefer_not_tell_1))
+        
+        #count Exp-2
+        females_2 = self.df_2[self.df_2.gender==0].shape[0]
+        males_2 = self.df_2[self.df_2.gender==1].shape[0]
+        prefer_not_tell_2 = self.df_2[self.df_2.gender==2].shape[0]
+        other_2 = self.df_2[self.df_2.gender==3].shape[0]
+    
+        print("Experiment-2 results")
+        print("females="+str(females_2))
+        print("males="+str(males_2))
+        print("prefer_not_tell="+str(prefer_not_tell_2))
+        print("other="+str(other_2))
+        
+        
+        print("size q_df1="+str(self.df_1.shape[0]))
+        print("size q_df2="+str(self.df_2.shape[0]))
+        
+        '''
+        Exp1 gender numbers do not do add to 777 because participants quit 
+        before answering the demographics survey.
+        '''
+        
+    #def compute_gender_age_distribution(self):
+           
+    def load_all_E2_gender_data(self):
+        '''
+        load csv with all the data from qualified and not qualified participants
+        '''
+        file_path = 'C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/'
+        csv_file = file_path  + 'gender_data.csv'
+        
+        df = pd.read_csv(csv_file)
+        print(df.head(10))
+        print(df.columns.values)
+
+        statTest = StatisticalSignificanceTest()
+        
+        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
+        print("series_1 = Male, series_2 = Female")
+        for profession in profession_list:
+            df_profession = df[df.Profession == profession]
+            df_female =  df_profession[df_profession['Worker Gender'] ==  'Female']
+            df_male = df_profession[df_profession['Worker Gender'] == 'Male']
+            print(profession)
+            statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])
+            
+        print("All")
+        df_female =  df[df['Worker Gender'] ==  'Female']
+        df_male = df[df['Worker Gender'] == 'Male']
+        statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])
+        
+genderStats = GenderStatistics()
+genderStats.load_all_E2_gender_data()      
