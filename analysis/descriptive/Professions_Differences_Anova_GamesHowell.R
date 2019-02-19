f4fdc60669c3e82fe023e31a8d463778903d8c3c
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-19 14:01:02 +0100

    Update Professions_Differences_Anova_GamesHowell.R
    Age of Male participants across professions
    ### Oneway Anova for y=age and x=experience (groups: Graduate_Student, Hobbyist, Other, Professional_Developer, Undergraduate_Student)
    
    Omega squared: 95% CI = [.32; .38], point estimate = .35
    Eta Squared: 95% CI = [.33; .38], point estimate = .36
                                          SS   Df       MS      F     p
    Between groups (error + effect)  59950.58    4 14987.64 267.92 <.001
    Within groups (error only)      108803.53 1945    55.94

--- analysis/descriptive/Professions_Differences_Anova_GamesHowell.R
+++ analysis/descriptive/Professions_Differences_Anova_GamesHowell.R
@@ -1,14 +1,19 @@
 # ANOVA Games-Howell Post-Hoc Test
 # https://rpubs.com/aaronsc32/games-howell-test
 
-#install.packages("userfriendlyscience")
-#install.packages("farff")
+install.packages("userfriendlyscience")
+install.packages("farff")
+install.packages("ufs")
+library(ufs)
 library(userfriendlyscience)
 library(farff)
 library(ggplot2)
 
 file_path <-  "C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//data//consolidated_Final_Experiment_2.arff"
 df <-  readARFF(file_path)
+
+#Replace Other * experience to simply other.
+df[grep("Other",df$experience),"experience"] <-"Other"
 
 df_male <-  df[df$gender == "Male",]
 df_female <-  df[df$gender == "Female",]
@@ -19,4 +24,11 @@ names(male_profession_ages_list) <- profession_names
 for(profession in profession_names){
   male_profession_ages_list[[profession]] = df_male[grep(profession,df_male$experience),"age"]
 }
+
+
+df_male_data = df_male[,c("experience","age")]
+df_male_data$experience <- as.factor(df_male_data$experience)
+
+one.way <- oneway(df_male_data$experience, y =df_male_data$age , posthoc = 'games-howell')
+one.way
 
