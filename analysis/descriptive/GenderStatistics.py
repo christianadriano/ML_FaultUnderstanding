6b084af6c8bc3abb6fa1bc670d5c4d327161acb6
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-20 12:02:20 +0100

    Separated methods about professions

--- analysis/descriptive/GenderStatistics.py
+++ analysis/descriptive/GenderStatistics.py
@@ -6,7 +6,6 @@ Created on Feb 18, 2019
 
 from util._file_loader import FileLoader
 from util.StatisticalSignificanceTest import StatisticalSignificanceTest
-import pandas as pd
 
 class GenderStatistics(object):
     '''
@@ -59,7 +58,6 @@ class GenderStatistics(object):
         print("prefer_not_tell="+str(prefer_not_tell_2))
         print("other="+str(other_2))
         
-        
         print("size q_df1="+str(self.df_1.shape[0]))
         print("size q_df2="+str(self.df_2.shape[0]))
         
@@ -68,62 +66,24 @@ class GenderStatistics(object):
         before answering the demographics survey.
         '''
         
-    def eval_gender_age_profession_distribution(self):
+    def eval_gender_age_distribution(self):
         '''
-        Compute gender age differences across professions for E1 and E2
+        Compute gender age differences for E1 and E2
         '''
         
         statTest = StatisticalSignificanceTest()
         
-        #E1
+        print("E1 All")
         df_female = self.df_1[self.df_1.gender == 0]
         df_male = self.df_1[self.df_1.gender == 1]
-        #print("E1 gender ages")
-        #statTest.statistical_test_averages(df_male.age,df_female.age)
-        
-        #E2
-        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
-        print("series_1 = Male, series_2 = Female")
-        for profession in profession_list:
-            df_profession = self.df_2[self.df_2.experience.str.contains(profession)]
-            df_female =  df_profession[df_profession.gender ==  0]
-            df_male = df_profession[df_profession.gender == 1]
-            #print(profession)
-            #statTest.statistical_test_averages(df_male.age,df_female.age)
+        statTest.statistical_test_averages(df_male.age,df_female.age)
             
         print("E2 All")
         df_female = self.df_2[self.df_2.gender == 0]
         df_male = self.df_2[self.df_2.gender == 1]
         statTest.statistical_test_averages(df_male.age,df_female.age)
-    
            
-    def eval_prequalified_gender_profession_age_differences(self):
-        '''
-        Only for E2. load csv with all the data from participants before they were qualified
-        Test for differences of age across profession and gender
-        '''
-        file_path = 'C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/'
-        csv_file = file_path  + 'gender_data.csv'
-        
-        df = pd.read_csv(csv_file)
-        print(df.head(10))
-        print(df.columns.values)
 
-        statTest = StatisticalSignificanceTest()
-        
-        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
-        print("series_1 = Male, series_2 = Female")
-        for profession in profession_list:
-            df_profession = df[df.Profession == profession]
-            df_female =  df_profession[df_profession['Worker Gender'] ==  'Female']
-            df_male = df_profession[df_profession['Worker Gender'] == 'Male']
-            print(profession)
-            statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])
-            
-        print("All")
-        df_female =  df[df['Worker Gender'] ==  'Female']
-        df_male = df[df['Worker Gender'] == 'Male']
-        statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])
         
 genderStats = GenderStatistics()
 #genderStats.load_all_E2_gender_data() 
---
+++ analysis/descriptive/ProfessionStatistics.py
@@ -0,0 +1,83 @@
+'''
+Created on Feb 20, 2019
+
+@author: Christian
+'''
+import pandas as pd
+from util._file_loader import FileLoader
+from util.StatisticalSignificanceTest import StatisticalSignificanceTest
+
+class ProfessionStatistics(object):
+    '''
+    Compute differences across professions in E2.
+    There was no information about profession in E1.
+    '''
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        loader = FileLoader()
+        self.df_2 = loader._load_file_2()
+        
+        
+    def eval_gender_profession_distribution(self):
+        '''
+        Compute gender differences across for E2
+        '''
+        
+        statTest = StatisticalSignificanceTest()
+
+        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
+        print("Gender by profession, series_1 = Male, series_2 = Female")
+        for profession in profession_list:
+            df_profession = self.df_2[self.df_2.experience.str.contains(profession)]
+            df_female =  df_profession[df_profession.gender ==  0]
+            df_male = df_profession[df_profession.gender == 1]
+            print(profession)
+            statTest.statistical_test_averages(df_male.age,df_female.age)
+        
+    def eval_gender_age_profession_distribution(self):
+        '''
+        Compute gender differences across for E2
+        '''
+        
+        statTest = StatisticalSignificanceTest()
+
+        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
+        print("series_1 = Male, series_2 = Female")
+        for profession in profession_list:
+            df_profession = self.df_2[self.df_2.experience.str.contains(profession)]
+            df_female =  df_profession[df_profession.gender ==  0]
+            df_male = df_profession[df_profession.gender == 1]
+            print(profession)
+            statTest.statistical_test_averages(df_male.age,df_female.age)
+            
+    def eval_prequalified_gender_profession_age_differences(self):
+        '''
+        Only for E2. load csv with all the data from participants before they were qualified
+        Test for differences of age across profession and gender
+        '''
+        file_path = 'C:/Users/Christian/Documents/GitHub/ML_FaultUnderstanding/data/'
+        csv_file = file_path  + 'gender_data.csv'
+        
+        df = pd.read_csv(csv_file)
+        print(df.head(10))
+        print(df.columns.values)
+
+        statTest = StatisticalSignificanceTest()
+        
+        profession_list = ["Professional_Developer","Hobbyist","Graduate_Student","Undergraduate_Student","Other"]
+        print("series_1 = Male, series_2 = Female")
+        for profession in profession_list:
+            df_profession = df[df.Profession == profession]
+            df_female =  df_profession[df_profession['Worker Gender'] ==  'Female']
+            df_male = df_profession[df_profession['Worker Gender'] == 'Male']
+            print(profession)
+            statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])
+            
+        print("All")
+        df_female =  df[df['Worker Gender'] ==  'Female']
+        df_male = df[df['Worker Gender'] == 'Male']
+        statTest.statistical_test_averages(df_male['Worker Age'],df_female['Worker Age'])   
+    
