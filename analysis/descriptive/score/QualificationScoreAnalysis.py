3e268b907f7de009ed500b91748a49c43fd1a168
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-02-24 21:40:42 +0100

    Create QualificationScoreAnalysis.py
    low, medium, high scores for E1
    [0.6924066924066924, 0.17245817245817247, 0.13513513513513514]

---
+++ analysis/descriptive/score/QualificationScoreAnalysis.py
@@ -0,0 +1,57 @@
+'''
+Created on Feb 24, 2019
+
+@author: Christian
+'''
+
+from util._file_loader import FileLoader
+from pywt._extensions._pywt import keep
+from click.decorators import group
+
+class QualificationScoreAnalysis(object):
+    '''
+    Analysis of qualification score 
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        loader = FileLoader()
+        self.df_1 = loader._load_file_1()
+        self.df_2 = loader._load_file_2()
+    
+    def distribution_qualification_scores(self):
+        '''
+        Distribution of scores for E1 and E2
+        Scores in E1 = 2,3,4
+        Scores in E2 = 3,4,5
+        '''   
+        #Remove repeated items
+        df_E1  =self.df_1.drop_duplicates(subset=['worker_id'],keep='last')
+        grouped_results = df_E1.groupby(['qualification_score'])
+        #print(grouped_results.agg(['size','count','unique']))
+        #Results
+        #(score,count) = (2,538); (3,134); (4,105)
+       
+        print(self.compute_score_percentages(grouped_results, [2,3,4]))
+        
+        #df_E2  =self.df_1.drop_duplicates(subset=['worker_id'],keep='last')
+        #grouped_results = df_E1.groupby(['qualification_score'])
+        #print(grouped_results.agg(['size','count','unique']))
+        #Results
+        #(score,count) = (2,538); (3,134); (4,105)
+
+    def compute_score_percentages(self,grouped_results, score_list):
+        
+        low = len(grouped_results.groups[score_list[0]])
+        medium = len(grouped_results.groups[score_list[1]])
+        high = len(grouped_results.groups[score_list[2]])
+        total = low + medium + high
+        
+        proportion_score = [x/total for x in [low,medium,high]]
+        return(proportion_score)
+         
+scoreAnalysis = QualificationScoreAnalysis()
+scoreAnalysis.distribution_qualification_scores()
