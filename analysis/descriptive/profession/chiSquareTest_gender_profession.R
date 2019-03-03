67dc23c0fc0522978aebccc3bf613039782afa90
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-03-03 12:48:40 +0100

    Update chiSquareTest_gender_profession.R
    clean up

--- analysis/descriptive/profession/chiSquareTest_gender_profession.R
+++ analysis/descriptive/profession/chiSquareTest_gender_profession.R
@@ -1,5 +1,6 @@
 #compute chi-square test for gender and professions
 
+#First way of doing it
 female_participants <- c(44,28,18,27,16);
 male_participants <-  c(125,83,45,89,20);
 participants <- rbind(female_participants,male_participants);
@@ -7,21 +8,12 @@ inv_part <- cbind(female_participants,male_participants);
 
 colnames(participants) <- c("ProfessionalDeveloper","Hobbyist","GraduateStudent","UndergraduateStudent","Other");
 rownames(participants) <- c("females","males");
-
 tbl <- as.table(participants)
-
-participants
 
 chisq = chisq.test(inv_part, correct = FALSE)
 chisq
 
-chisq = chisq.test(female_participants)
-chisq
-
-chisq = chisq.test(male_participants)
-chisq
-
-#why R chisq.test and Python Scipy.stats.chi2_contingency are giving different results?
+#Second way of doing it
 
 mat <- matrix(c(44,28,18,27,16,125,83,45,89,20),nrow = 5)
 tbl <- as.table(mat)
@@ -40,10 +32,13 @@ chitest
 #between Gender and Profession.
 
 #
-mat <- matrix(c(44,28,18,27,16,125,83,45,89),nrow = 4)
+mat <- matrix(c(44,28,18,27,125,83,45,89),nrow = 4)
 tbl <- as.table(mat)
 rownames(tbl) <- c("ProfessionalDeveloper","Hobbyist","GraduateStudent","UndergraduateStudent");
 colnames(tbl) <- c("females","males");
 tbl
 chitest <- chisq.test(tbl)
 chitest
+#X-squared = 0.64496, df = 3, p-value = 0.8861
+#Even after removing OTHER, there is no particular concentrantion of gender by profession.
+#i.e., genders seem to have similar proportions across professions, i.e., gender and profession are independent.
