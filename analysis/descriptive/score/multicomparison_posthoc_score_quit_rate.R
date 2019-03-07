ddf73a461f05af1a22e10fe4b1bca9b748949d55
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-03-07 12:20:41 +0100

    Three ways of group_by

--- analysis/descriptive/score/multicomparison_posthoc_score_quit_rate.R
+++ analysis/descriptive/score/multicomparison_posthoc_score_quit_rate.R
@@ -19,6 +19,12 @@ file_path <-  "C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//d
 df2 <-  readARFF(file_path)
 
 df1 <- select(df1,'worker_id','session_id','microtask_id')
-df_grouped <- group_by(list(df1['worker_id']),list(df1.['session_id']))
 
-                       
+df1 %>% group_by(session_id) %>% summarise(microtask_id= length(unique(microtask_id)))
+
+df_group <- ddply(df1,~session_id,summarise,tasks=length(unique(microtask_id)))
+
+#https://stats.stackexchange.com/questions/8225/how-to-summarize-data-by-group-in-r
+#https://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group
+#https://www.nature.com/articles/s41467-019-08806-w
+
