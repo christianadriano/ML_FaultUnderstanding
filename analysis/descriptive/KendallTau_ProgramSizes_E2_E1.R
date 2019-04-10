868e550d25cbe5b97f6655f39119b0397e0a4f25
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-04-10 18:30:37 +0200

    Update KendallTau_ProgramSizes_E2_E1.R
    Updated the number of Words (removed java doc markers)

--- analysis/descriptive/KendallTau_ProgramSizes_E2_E1.R
+++ analysis/descriptive/KendallTau_ProgramSizes_E2_E1.R
@@ -14,14 +14,14 @@ df2 <- read.csv(file_path,header = TRUE,sep=",")
 
 df2
 # bugID LOC Lines Words Characters
-#   1   23    54   337       2484
-#   2    7    13    58        438
-#   3   23    31   162       1249
+#   1   23    54   306       2484
+#   2    7    13    52        438
+#   3   23    31   154       1249
 #   4   78    78   241       2641
-#   5    7    33   266       1667
+#   5    7    33   240       1667
 #   6   28    28   106        665
-#   7   12    21   108        831
-#   8   33    62   332       2426
+#   7   12    21    99        831
+#   8   33    62   303       2426
 
 
 shapiro.test(df2$LOC)
@@ -31,7 +31,7 @@ shapiro.test(df2$Lines)
 #Normal, W = 0.93028, p-value = 0.5186
 
 shapiro.test(df2$Words)
-#Normal, W = 0.91483, p-value = 0.3893
+#W = 0.90901, p-value = 0.3471
 
 shapiro.test(df2$Characters)
 #Normal, W = 0.8937, p-value = 0.2532
@@ -41,16 +41,16 @@ cor.test(df2$LOC,df2$Lines,method=c("kendall"))
 # tau = 0.5929995 
 
 cor.test(df2$LOC,df2$Words,method=c("kendall"))
-# z = 0.75394, p-value = 0.4509
-# tau = 0.2223748 NOT Significant
+#z = 1.2566, p-value = 0.2089
+# tau = 0.3706247 NOT Significant
 
 cor.test(df2$LOC,df2$Characters,method=c("kendall"))
 # z = 1.5079, p-value = 0.1316
 # tau = 0.4447496 NOT Significant
 
 cor.test(df2$Lines,df2$Words,method=c("kendall"))
-# T = 23, p-value = 0.03115
-# tau = 0.6428571 
+# T = 25, p-value = 0.005506
+# tau = 0.7857143 
 
 cor.test(df2$Lines,df2$Characters,method=c("kendall"))
 # T = 26, p-value = 0.001736
@@ -61,14 +61,11 @@ cor.test(df2$Words,df2$Characters,method=c("kendall"))
 # tau = 0.7857143
 
 "
-LOC is not correlated with characters, or words. This means that 
+LOC is not correlated with Characters, or Words. This means that 
 when ranking the programs by LOC is different than ranking by 
-words or characters. Since Characters, Lines, and Words are 
+Words. or Characters Since Characters, Lines, and Words are 
 strongly correlated with each other, I would replace LOC size 
-measure with lines
-analyzes with LOC with
-with both Words and Lines, one could use Characters to have 
-
+measure with Lines or Words.
 "
 
 df2_plot <- select(df2,LOC,Lines,Words,Characters)
