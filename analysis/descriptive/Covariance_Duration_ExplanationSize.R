0a3fa3286a9d18aacbb7e4af544ee5e6c43e2aa4
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-05-02 19:29:01 +0200

    Update Covariance_Duration_ExplanationSize.R
    Duration and Explanation size are correlated. Medium strength for both E1 and E2.

--- analysis/descriptive/Covariance_Duration_ExplanationSize.R
+++ analysis/descriptive/Covariance_Duration_ExplanationSize.R
@@ -21,8 +21,14 @@ df2 <-
          'duration',
          'explanation')
 
+#Outlier treament
+upper_limit <- (5*60*1000) * 10 #50 min, which is 10 times the duration for which the task was designed for
+lower_limit <- (5*60*1000) / 10 #30 seconds, which is the minimum expected read test case, questions, and the program statement (3 lines)
+df2 <- df2[df2$duration<upper_limit & df2$duration>lower_limit,]
+
+
 df2['explanation_size'] <- nchar(df2$explanation)
-plot(df2$explanation_size)
+plot(df2$duration, df2$explanation_size)
 
 #remove outlier (above 1000 characters)
 df2 <- df2[df2$explanation_size<1000,]
@@ -42,4 +48,15 @@ plot(df1$explanation_size)
 
 #remove outlier (above 1000 characters)
 df1 <- df1[df1$explanation_size<1000,]
+df1 <- df1[df1$duration<upper_limit & df1$duration>lower_limit,]
 
+
+cor.test(df1$duration,df1$explanation_size,method="kendall")
+#z = 29.252, p-value < 2.2e-16, tau=0.3213421 (Medium correlation)
+cor.test(df1$duration,df1$explanation_size,method="pearson")
+#t = 21.793, df = 3713, p-value < 2.2e-16, 95 percent confidence interval: 0.3079331 0.3649627, pearson cor = 0.3367567 
+
+cor.test(df2$duration,df2$explanation_size,method="kendall")
+#z = 20.427, p-value < 2.2e-16, tau=0.2777974 (Medium correlation)
+cor.test(df2$duration,df2$explanation_size,method="pearson")
+#t = 16.133, df = 2414, p-value < 2.2e-16, 95 percent confidence interval: 0.2755117 0.3475179, Pearson cor =0.3119627 
