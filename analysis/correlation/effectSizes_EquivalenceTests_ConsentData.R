89d99227cc79afb779c79ac0fc811fc69b4f47b3
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-02-06 23:21:24 +0100

    Update effectSizes_EquivalenceTests_ConsentData.R

--- analysis/correlation/effectSizes_EquivalenceTests_ConsentData.R
+++ analysis/correlation/effectSizes_EquivalenceTests_ConsentData.R
@@ -2,8 +2,15 @@
 Test of Equivalence between two means.
 This is done after we fail to reject a null-hypothesis.
 
+Based on paper: Equivalence Testing for Psychological Research: A Tutorial
+DOI: 10.1177/2515245918770963
+
+Look at:
+https://reilly-lab.github.io/Equivalence_Tutorial.html
+
 TODO:
 - Compute correlation matrix
+- Plot matrix
 - For the non-significant correlations, run TOST (assuming effect less than small)
 
 
