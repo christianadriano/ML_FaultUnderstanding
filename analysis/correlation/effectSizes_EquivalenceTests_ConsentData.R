89c99cc0c19f924e743366026d86b8a3761d9984
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-02-07 23:18:13 +0100

    Update effectSizes_EquivalenceTests_ConsentData.R

--- analysis/correlation/effectSizes_EquivalenceTests_ConsentData.R
+++ analysis/correlation/effectSizes_EquivalenceTests_ConsentData.R
@@ -15,9 +15,50 @@ TODO:
 
 
 "
-install.packages("TOSTER")
+#install.packages("TOSTER")
 library(TOSTER)
+library(dplyr)
+install.packages("Hmisc") #to compute the correlations with the significance levels
+library(Hmisc)
+install.packages("corrplot")
+library(corrplot)
+
+#----------------------------------------------
 
 "Load data with treatment field (isBugCovering) and ground truth (answer correct)"
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
 
+df_consent$profession_level <- as.numeric(df_consent$profession_id)
+df_data$profession_level <- 7-df_data$profession_level #so it is incremental (higher skill, larger level)
+
+#compute correlations between qualification_score, adjusted_score, profession_level,test_duration, age, years_programming 
+df_data <- df_consent%>%select(qualification_score, adjusted_score, profession_level,test_duration, age, years_programming)
+colnames(df_data)
+#using Spearman because all data sets failed the Shapiro-Wilk normality test (p-value<0.05)
+corr_matrix <- rcorr(as.matrix(df_data),type="spearman")
+corr_matrix
+
+#The only two non-significant correlations are:
+#   row                column   cor            p
+# qualification_score   age  0.03906618 9.866124e-02
+# adjusted_score        age  0.03805823 1.076732e-01
+
+# ++++++++++++++++++++++++++++
+# flattenCorrMatrix
+# source: http://www.sthda.com/english/wiki/correlation-matrix-a-quick-start-guide-to-analyze-format-and-visualize-a-correlation-matrix-using-r-software
+# ++++++++++++++++++++++++++++
+# cormat : matrix of the correlation coefficients
+# pmat : matrix of the correlation p-values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+flattenCorrMatrix(corr_matrix$r, corr_matrix$P)
+
+corrplot(corr_matrix$r, type = "upper", order = "hclust", 
+        tl.col = "black", tl.srt = 45)
