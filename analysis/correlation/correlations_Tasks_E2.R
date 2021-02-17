136c107e29cfe15f710fc3f9acdaf07d4f1fbc1d
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-02-17 23:20:15 +0100

    Create correlations_Tasks_E2.R

---
+++ analysis/correlation/correlations_Tasks_E2.R
@@ -0,0 +1,148 @@
+"
+Correlations in Tasks and Demographics attributes from E2
+
+TODO:
+ - Compute correlation matrix
+ - Plot matrix
+ - For the non-significant correlations, run TOST (assuming effect less than small)
+
+"
+#install.packages("TOSTER")
+library(TOSTER)
+library(dplyr)
+#install.packages("Hmisc") #to compute the correlations with the significance levels
+library(Hmisc)
+#install.packages("corrplot")
+library(corrplot)
+
+#----------------------------------------------
+
+"Load data with treatment field (isBugCovering) and ground truth (answer correct)"
+source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_ground_truth_E2.R")
+
+df_E2_ground$profession_level <- as.numeric(df_consent$profession_id)
+
+#compute correlations between qualification_score, adjusted_score, profession_level,test_duration, age, years_programming 
+df_data <- df_E2_ground%>%select(adjusted_score, profession_level,test_duration, age, years_programming, durat)
+df_data$profession_level <- 7-df_data$profession_level #so it is incremental (higher skill, larger level)
+
+#--------------------------------------
+#CORRELATION MATRIX
+
+#Using Spearman because all data sets failed the Shapiro-Wilk normality test (p-value<0.05)
+#corr_matrix <- cor.test(df_data$adjusted_score,df_data$profession_level,method=c("kendall"))
+#corr_matrix$estimate
+# mat : is a matrix of data
+# ... : further arguments to pass to the native R cor.test function
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  r.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  diag(r.mat) <- 1
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], method=c("kendall"), ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+      r.mat[i, j] <- r.mat[j, i] <- tmp$estimate
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  colnames(r.mat) <- rownames(r.mat) <- colnames(mat)
+  return(list(r.mat,p.mat))
+}
+
+
+# matrix of the p-value of the correlation
+list_mat <- cor.mtest(df_data)
+r.mat <- list_mat[[1]]
+p.mat <- list_mat[[2]]
+head(p.mat)
+head(r.mat)
+
+
+#The only two non-significant correlations are:
+#   row                column   cor            p
+# qualification_score   age  0.03906618 9.866124e-02
+# adjusted_score        age  0.03805823 1.076732e-01
+
+
+#EQUIVALENCE TEST
+#Documentation https://github.com/Lakens/TOSTER/blob/master/tests/testthat/test-data_summary_equivalent.R
+
+results <- dataTOSTr(data=df_data,
+                     pairs=list(list(i1="adjusted_score",i2="age")),
+                          low_eqbound_r = -0.3, 
+                          high_eqbound_r = 0.3, alpha=0.05, 
+                          desc=TRUE, plots=TRUE)
+
+results
+# -------------------------------------------------------------------
+# TOST Results                              r             p            
+# -------------------------------------------------------------------
+# adjusted_score    age    Pearson's r    0.03558930     0.1325019   
+#                          TOST Upper     0.03558930    < .0000001   
+#                          TOST Lower     0.03558930    < .0000001  
+# -------------------------------------------------------------------
+# The p-values for Upper and Lower show that the correlation is bettwen lower and upper.
+
+# -----------------------------------------------------------------------------------
+#                                                       90% confidence interval
+# -----------------------------------------------------------------------------------
+# Equivalence Bounds          Low           High         Lower           Upper        
+# -----------------------------------------------------------------------------------
+# adjusted_score    age    -0.3000000    0.3000000    -0.003327777    0.07439874   
+# -----------------------------------------------------------------------------------
+#Correlation CI crosses zero, hence the correlation is zero.
+
+#---------------------------------------------------------------------------          
+
+
+# ++++++++++++++++++++++++++++
+# flattenCorrMatrix
+# source: http://www.sthda.com/english/wiki/correlation-matrix-a-quick-start-guide-to-analyze-format-and-visualize-a-correlation-matrix-using-r-software
+# ++++++++++++++++++++++++++++
+# cormat : matrix of the correlation coefficients
+# pmat : matrix of the correlation p-values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+f <- flattenCorrMatrix(r.mat, p.mat)
+write.csv(f,"C://Users//Christian//Documents//GitHub//ML_FaultUnderstanding//analysis//correlation//output//Consent_KendallCorrelation_E2.csv")
+
+#----------------------------------------------------------------------
+col<- colorRampPalette(c("red", "grey", "blue"))(20)
+corrplot(r.mat, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45, method="number", col=col)
+
+
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(r.mat, method="color", col=col(200),  
+         type="upper", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.05, insig = "blank", 
+         # hide correlation coefficient on the principal diagonal
+         diag=FALSE 
+)
+
+#--------------------------------------------------------
+#Hybrid PLOT
+
+#install.packages("PerformanceAnalytics")
+library("PerformanceAnalytics")
+chart.Correlation(df_data, histogram=TRUE, pch="+", method="kendall")
+
+#--------------------------------------------------------
+#HEAT MAP (Not USED)
+
+col<- colorRampPalette(c("blue", "white", "red"))(20)
+heatmap(x = r.mat, col = col, symm = TRUE)
