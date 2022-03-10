405e7fea4458ce75c8e542b52ac6892ca2fa05a6
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2022-03-10 23:44:47 +0100

    Create equivalenceTest_example.R

---
+++ analysis/correlation/equivalenceTest_example.R
@@ -0,0 +1,10 @@
+"
+Test of Equivalence between two means.
+This is done after we fail to reject a null-hypothesis.
+
+Based on paper: Equivalence Testing for Psychological Research: A Tutorial
+DOI: 10.1177/2515245918770963
+
+Look at:
+  https://reilly-lab.github.io/Equivalence_Tutorial.html
+"
